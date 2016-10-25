c99ee067e212732465ba1870064b693eec199a68
Jeroen Jonker <jeroenojonker@hotmail.nl>
2016-10-25 13:46:00 +0200

    cleanup

--- js/mazemap/Cell.js
+++ js/mazemap/Cell.js
@@ -4,7 +4,9 @@ class cell
     {
         this.positionx = i;
         this.positiony = j;
+        // alleen voor generator
         this.unvisited = true;
+        // walldirections ["noord","oost","zuid","west"]
         this.walls = [true,true,true,true];
     }
 }
--- js/mazemap/mazegenerator.js
+++ js/mazemap/mazegenerator.js
@@ -1,5 +1,7 @@
+//--- MAZE GENERATOR----
+//the cells of the maze
 var cells = [];
-var size = 15;
+var size = 20;
 for (i = 0; i < size; i++)
 {
     for (j = 0; j < size; j++)
@@ -8,71 +10,63 @@ for (i = 0; i < size; i++)
         }
 }
 
-cells[0].unvisited = false;
-var route = [cells[0]];
-var currentcell = 0;
+cells[((size*size)/2)].unvisited = false;
+var route = [cells[((size*size)/2)]];
+var currentcell = ((size*size)/2);
 var steps = [-size, 1, size,-1];
 var seed = 7359;
-var directionsstrings = ["noord","oost","zuid","west"];
 movedirection(currentcell,0);
 
-for (var x = 0; x < cells.length; x++)
-{
-    if (cells[x].unvisited != true)
-    {
-        console.log(cells[x].unvisited);
-    }
-}
-
+// Makes a route out of the cells
 function movedirection(currentcellnr, currentroutenr)
 {
+    // Directions ["noord","oost","zuid","west"]
     var directions = [0,1,2,3];
     for (var i = 0; i < 4; i++)
         {
             var direction = directions[Math.round(random() * (directions.length -1))];
             var newcurrentcellnr = currentcellnr + steps[direction];
             
-            
+            // Checks if not of out of range on y and x or already visited
             if (cells[newcurrentcellnr] != undefined && cells[newcurrentcellnr].unvisited && !((cells[currentcellnr].positionx % size == 0 && cells[newcurrentcellnr].positionx % size == (size -1)) || (cells[currentcellnr].positionx % size == (size -1) && cells[newcurrentcellnr].positionx % size == 0)))
             {
-                console.log(currentcellnr + " " + directionsstrings[direction]);
                 var newcurrentroutenr = currentroutenr + 1;
                 if (direction < 2)
-                    {
-                        cells[newcurrentcellnr].walls[(direction +2)] = false;
-                    }
+                {
+                    cells[newcurrentcellnr].walls[(direction +2)] = false;
+                }
                 else
-                    {
-                        cells[newcurrentcellnr].walls[(direction -2)] = false;
-                    }
+                {
+                    cells[newcurrentcellnr].walls[(direction -2)] = false;
+                }
                 cells[currentcellnr].walls[direction] = false;
                 cells[newcurrentcellnr].unvisited = false;
                 route.push(cells[newcurrentcellnr]);
-                console.log(newcurrentcellnr);
                 movedirection(newcurrentcellnr, newcurrentroutenr);
             }
             directions = RemoveElement(directions,direction);  
         }
         route.pop();
-        console.log("NOPE! TERUG!");
         return;
 }
 
-function random() {
+// Uses seed for predictable outcome
+function random() 
+{
     var x = Math.sin(seed++) * 10000;
     return x - Math.floor(x);
 }
 
-
+//Removes element from array
 function RemoveElement(oldarray, nr)
 {
     var k = [];
     for (var m = 0; m < oldarray.length; m++)
-        {
-            if (oldarray[m] != nr)
-                {
-                    k.push(oldarray[m]);
-                }
-        }
+    {
+        if (oldarray[m] != nr)
+            {
+                k.push(oldarray[m]);
+            }
+    }
     return k;
 }
